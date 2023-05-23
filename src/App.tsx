abeafb55e2d1a7d60fd118adb16f422db9b69802
CamillePB <camille.pedro.bueno@hotmail.com>
2023-05-23 14:09:41 -0300

    get, post e put em Tema e Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import Login from './paginas/login/Login'
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario'
 import ListaTema from './components/tema/listatema/ListaTema'
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem'
+import CadastroPostagem from './components/postagens/cadastropostagem/CadastroPostagem'
+import CadastroTema from './components/tema/cadastrotema/CadastroTema'
+import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
+import DeletarTema from './components/tema/deletartema/DeletarTema'
 
 
 function App() {
@@ -14,16 +18,29 @@ function App() {
     <>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          {/* <div style={{minHeight: '100vh'}}> */}
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
-          <Route path="/temas" element={<ListaTema />} />
-          <Route path="/posts" element={<ListaPostagem />} />
-          {/* </div> */}
-        </Routes>
+        <div style={{ minHeight: '100vh' }}>
+          <Routes>
+
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+            <Route path="/temas" element={<ListaTema />} />
+            <Route path="/posts" element={<ListaPostagem />} />
+            <Route path="/formularioPostagem" element={<CadastroPostagem />} />
+
+            <Route path="/formularioPostagem/:id" element={<CadastroPostagem />} />
+
+            <Route path="/formularioTema" element={<CadastroTema />} />
+
+            <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+            <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+            <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            {/* </div> */}
+          </Routes>
+        </div>
         <Footer />
       </BrowserRouter>
     </>
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -2,57 +2,66 @@ import react from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import { Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom'
+import useLocalStorage from 'react-use-localstorage';
 import "./Navbar.css";
 
-function Navbar() {
-
+    function Navbar() {
+        //função de zerar o token
+        const [token, setToken] = useLocalStorage('token');
+        let navigate = useNavigate();
+        
+        //chama a função de token vazio
+        function goLogout(){
+            setToken('')
+            alert("Usuário deslogado")
+           navigate('/login')
+        }
     return (
         <>
             <AppBar position="static" style={{ backgroundColor: "#7e1f1f" }}>
                 <Toolbar variant="dense">
+                <Link to='/home' className='text-decorator-none'>
                     <Box style={{ cursor: "pointer" }}>
                             <Typography variant="h5" color="inherit">
                                 BlogPessoal
                             </Typography>
-                        
-
                     </Box>
+                    </Link>
 
                     <Box display="flex" justifyContent="start">
-                        <Link to='/home' className='text-decorator-none'>
-                            <Box mx={1} style={{ cursor: "pointer" }} marginLeft={60}>
-                                <Typography variant="h6" color="inherit">
-                                    home
+
+                        <Box mx={1} style={{ cursor: "pointer" }}  marginLeft={75}>
+                            <Typography style={{ color: "White" }}>
+                                CRIAR POSTAGEM
+                            </Typography>
+                        </Box>
+                        <Link to='/posts' className='text-decorator-none'>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography color="inherit">
+                                POSTAGENS
+                            </Typography>
+                        </Box>
+                        </Link>
+                        <Link to='/temas' className='text-decorator-none'>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography color="inherit">
+                                TEMAS
+                            </Typography>
+                        </Box>
+                        </Link>
+                        <Link to='/formularioTema' className='text-decorator-none'>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography color="inherit">
+                                CRIAR TEMA
+                            </Typography>
+                        </Box>
+                        </Link>
+                            <Box mx={1} style={{ cursor: "pointer" }} onClick={goLogout}>
+                                <Typography color="inherit">
+                                    SAIR
                                 </Typography>
                             </Box>
-                        </Link>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" style={{ color: "White" }}>
-                                criar postagem
-                            </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                postagens
-                            </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                temas
-                            </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                cadastrar tema
-                            </Typography>
-                        </Box>
-                        <Link to='/login' className='text-decorator-none'>
-                            <Box mx={1} style={{ cursor: "pointer" }}>
-                                <Typography variant="h6" color="inherit">
-                                    logout
-                                </Typography>
-                            </Box>
-                        </Link>
                     </Box>
 
                 </Toolbar>
